054cec286019522c4b5da14182cb6c6b1949ec55
Amit singh <amitsingh10d@gmail.com>
2023-05-04 07:49:26 +0530

    Created user routes

--- routes/urls.js
+++ routes/urls.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const AdminController = require('controller/admin.controller')
+const AdminController = require('../controller/admin.controller')
+const UserController = require('../controller/customer.controller')
+
 const router = express.Router()
 
 const adminController = new AdminController();
 adminController.register(router)
+
+const userController = new UserController();
+userController.register(router)
 
 module.exports = router;
